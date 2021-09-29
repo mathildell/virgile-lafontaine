a094568ea3214d5172cdb9ac0042c185c663c791
Mathilde Luce-Lucas <mluce-lucas@lawebshop.ca>
2021-09-28 23:55:02 -0400

    hey

--- config/initializers/website_config.rb
+++ config/initializers/website_config.rb
@@ -1,8 +1,7 @@
 Rails.application.config.to_prepare do
-  WebsiteConfig.create!( website_name: "App de Virgile",
-                         artist_name: "Virgile",
-                         show_projects_links: true) unless WebsiteConfig.any?
-
+  # WebsiteConfig.create!( website_name: "App de Virgile",
+  #                        artist_name: "Virgile",
+  #                        show_projects_links: true) unless WebsiteConfig.any?
   $website_config = WebsiteConfig.first
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+WebsiteConfig.create!( website_name: "App de Virgile", show_projects_links: true) unless WebsiteConfig.any?
+Admin.create!( { "email"=>"mathildelucelucas@gmail.com",
+                "encrypted_password"=>"$2a$12$S/VrY3YnardlxdjS2jdFd.EWA.yFTwYmIW0X7iJqWr5HbBEOV3Wcy",
+                "name"=>"Mathilde"})
